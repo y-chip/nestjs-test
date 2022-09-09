3ae4fe1d28d43fdc8544e029b975ebd68108adc0
Yuta Yamamoto <wookiee33@gmail.com>
2022-09-10 00:12:35 +0900

    CatsControllerを追加する。

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  Req,
+} from "@nestjs/common";
+import { CatDto } from "./cat.dto";
+
+@Controller("cats")
+export class CatsController {
+  @Post()
+  create(@Body() catDto: CatDto) {
+    return "This action adds a new cat";
+  }
+
+  @Get()
+  findAll() {
+    return `This action returns all cats`;
+  }
+
+  @Get(":id")
+  findOne(@Param("id") id: string) {
+    return `This action returns a #${id} cat`;
+  }
+
+  @Put(":id")
+  update(@Param("id") id: string, @Body() catDto: CatDto) {
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(":id")
+  remove(@Param("id") id: string) {
+    return `This action removes a #${id} cat`;
+  }
+}
