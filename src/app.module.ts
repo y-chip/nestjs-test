b8584ebb6a585976557a9aefebb9f2feefeaf51b
Yuta Yamamoto <wookiee33@gmail.com>
2022-09-10 09:15:59 +0900

    アップロードしたファイルを保管する。

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,33 @@ import { CatsController } from "./cats/cats.controller";
 import { FilesController } from "./files/files.controller";
 import { ScheduleModule } from "@nestjs/schedule";
 import { TasksService } from "./tasks/tasks.service";
+import { MulterModule } from "@nestjs/platform-express";
+import { diskStorage } from "multer";
+import * as dayjs from "dayjs";
+
+const crypto = require("crypto");
+const fs = require("fs");
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [
+    ScheduleModule.forRoot(),
+    MulterModule.register({
+      storage: diskStorage({
+        destination: (req, file, callback) => {
+          const identifier =
+            dayjs().format("YYYYMMDD_HHmmss") + "_" + crypto.randomUUID();
+          const directory = `./resource/upload/${identifier}`;
+
+          fs.mkdirSync(directory, { recursive: true });
+
+          callback(null, directory);
+        },
+        filename: (req, file, callback) => {
+          callback(null, file.originalname);
+        },
+      }),
+    }),
+  ],
   controllers: [SamplesController, CatsController, FilesController],
   providers: [SamplesService, TasksService],
 })
